9c33a1a53167f8116c9ffc9de360193da1470b5e
sentense1 <3xsaint@gmail.com>
2023-09-30 12:18:19 +0100

    eslint fixed

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,12 @@
 export default function* createIteratorObject(report) {
-  const allEmployees = report.allEmployees;
+  const { allEmployees } = report; // Use object destructuring
 
   for (const department in allEmployees) {
-    const employeesInDepartment = allEmployees[department];
-    for (const employee of employeesInDepartment) {
-      yield employee;
+    if (allEmployees.hasOwnProperty(department)) {
+      const employeesInDepartment = allEmployees[department];
+      for (const employee of employeesInDepartment) {
+        yield employee;
+      }
     }
   }
 }
