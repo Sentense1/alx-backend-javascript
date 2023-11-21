724c2e874021064d779bbe69bdc27f9dc8313767
johnson-oragui <johnson.oragui@gmail.com>
2023-11-21 19:24:26 +0000

    modified to remove deprecated syntax

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,70 +1,42 @@
-const express = require('express');
+import readDatabase from '../utils';
 
-const args = process.argv.slice(2);
-const countStudents = require('./3-read_file_async');
+class StudentsController {
+  static getAllStudents(request, response, DATABASE) {
+    readDatabase(DATABASE)
+      .then((fields) => {
+        const students = [];
+        students.push('This is the list of our students');
 
-const DATABASE = args[0];
+        for (const key of Object.keys(fields)) {
+          msg = `Number of students in ${key}: ${
+            fields[key].length
+          }. List: ${fields[key].join(', ')}`;
 
-const app = express();
-const port = 1245;
+          students.push(msg);
+        }
+	response.status(200).send(`${students.join('\n')}`);
+      })
+      .catch(() => {
+        response.send(500, 'Cannot load the database');
+	response.status(500).send('Cannot load the database');
+      });
+  }
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');import readDatabase from '../utils';
+  static getAllStudentsByMajor(request, response, DATABASE) {
+    const { major } = request.params;
 
-  class StudentsController {
-    static getAllStudents(request, response, DATABASE) {
+    if (major !== 'CS' && major !== 'SWE') {
+      response.send(500, 'Major parameter must be CS or SWE');
+    } else {
       readDatabase(DATABASE)
         .then((fields) => {
-          const students = [];
-          let msg;
-  
-          students.push('This is the list of our students');
-  
-          for (const key of Object.keys(fields)) {
-            msg = `Number of students in ${key}: ${
-              fields[key].length
-            }. List: ${fields[key].join(', ')}`;
-  
-            students.push(msg);
-          }
-          response.send(200, `${students.join('\n')}`);
+          const students = fields[major];
+
+	  response.status(200).send(`List: ${students.join(', ')}`);
         })
-        .catch(() => {
-          response.send(500, 'Cannot load the database');
-        });
-    }
-  
-    static getAllStudentsByMajor(request, response, DATABASE) {
-      const { major } = request.params;
-  
-      if (major !== 'CS' && major !== 'SWE') {
-        response.send(500, 'Major parameter must be CS or SWE');
-      } else {
-        readDatabase(DATABASE)
-          .then((fields) => {
-            const students = fields[major];
-  
-            response.send(200, `List: ${students.join(', ')}`);
-          })
-          .catch(() => response.send(500, 'Cannot load the database'));
-      }
+        .catch(() => response.status(500).send('Cannot load the database'));
     }
   }
-  
-  export default StudentsController;
-});
+}
 
-app.get('/students', async (req, res) => {
-  const msg = 'This is the list of our students\n';
-  try {
-    const students = await countStudents(DATABASE);
-    res.send(`${msg}${students.join('\n')}`);
-  } catch (error) {
-    res.send(`${msg}${error.message}`);
-  }
-});
-
-app.listen(port, () => {
-});
-
-module.exports = app;
+export default StudentsController;
