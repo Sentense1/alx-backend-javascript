54535afcc09b8f8e5124dc00111ea7d30d737e8c
johnson-oragui <johnson-oragui@gmail.com>
2023-12-02 11:52:33 +0100

    fixing

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const x = uploadPhoto();
-  const y = createUser();
-  return Promise.all([x, y]).then(
-    (data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`),
-    () => console.log('Signup system offline'),
-  );
+
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`)})
+    .catch((error) => console.log('Signup system offline'));
 }
+
+handleProfileSignup();
