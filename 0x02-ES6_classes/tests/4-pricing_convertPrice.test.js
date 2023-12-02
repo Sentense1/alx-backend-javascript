a244bb787a679229a8be3e8bd0e645d4b00aec51
johnson-oragui <johnson-oragui@gmail.com>
2023-12-02 17:32:32 +0100

    test 4

--- 0x02-ES6_classes/tests/4-pricing_convertPrice.test.js
+++ 0x02-ES6_classes/tests/4-pricing_convertPrice.test.js
@@ -1,4 +1,4 @@
-import Pricing from './4-pricing.js';
+import Pricing from '../4-pricing.js';
 
 test("convertPrice is implemented correctly", () => {
     expect(Pricing.convertPrice(100, 2)).toBe(200);
