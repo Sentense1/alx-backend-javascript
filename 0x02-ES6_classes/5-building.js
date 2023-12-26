e9b83e85fbf166ce6542ec1e710b64a9edfddd7e
johnson-oragui <johnson-oragui@gmail.com>
2023-12-26 12:28:31 +0100

    eslint

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -20,6 +20,7 @@ export default class Building {
     this._sqft = Number(sqft);
   }
 
+  // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
