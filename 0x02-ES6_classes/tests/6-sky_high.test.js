fa2765736a4b01aea9502c83f16612abe31b6ca6
johnson-oragui <johnson-oragui@gmail.com>
2023-12-26 13:24:34 +0100

    task 6

--- 0x02-ES6_classes/tests/6-sky_high.test.js
+++ 0x02-ES6_classes/tests/6-sky_high.test.js
@@ -1,11 +1,11 @@
-import SkyHighBuilding from './6-sky_high.js';
+import SkyHighBuilding from '../6-sky_high';
 
-test("SkyHighBuilding is implemented correctly", () => {
-    const building = new SkyHighBuilding(200, 65);
-  
-    expect(building.sqft).toBe(200);
-    expect(building.floors).toBe(65);
-  
-    expect(typeof building.evacuationWarningMessage).toBe("function");
-    expect(building.evacuationWarningMessage()).toBe("Evacuate slowly the 65 floors");
+test('skyHighBuilding is implemented correctly', () => {
+  const building = new SkyHighBuilding(200, 65);
+
+  expect(building.sqft).toBe(200);
+  expect(building.floors).toBe(65);
+
+  expect(typeof building.evacuationWarningMessage).toBe('function');
+  expect(building.evacuationWarningMessage()).toBe('Evacuate slowly the 65 floors');
 });
