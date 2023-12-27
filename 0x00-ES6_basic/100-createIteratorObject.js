9674848de48edd5d19bcc35cbb1b5ed450e970ad
johnson-oragui <johnson-oragui@gmail.com>
2023-12-27 16:45:56 +0100

    tests file, and modified to call hasOwnProperty from Object

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,7 +2,7 @@ export default function* createIteratorObject(report) {
   const { allEmployees } = report; // Use object destructuring
 
   for (const department in allEmployees) {
-    if (allEmployees.hasOwnProperty(department)) {
+    if (Object.prototype.hasOwnProperty.call(allEmployees, department)) {
       const employeesInDepartment = allEmployees[department];
       for (const employee of employeesInDepartment) {
         yield employee;
