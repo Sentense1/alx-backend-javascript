f5470301cc3d80ca2b0bc9a6c790646bc4bc1112
Johnson Oragui <johnson.oragui@gmail.com>
2024-02-07 13:24:23 +0100

    Update 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -8,12 +8,12 @@ async function countStudents(Path) {
         reject(new Error('Cannot load the database'));
         return;
       }
-      
+
       const fileData = data.split('\n').filter((line) => line !== '');
 
       const csStudents = fileData.filter((line) => {
         const elements = line.split(',');
-        return elements.length >=4 && elements[3].trim() === 'CS';
+        return elements.length >= 4 && elements[3].trim() === 'CS';
       });
 
       const sweStudents = fileData.filter((line) => line.split(',')[3].trim() === 'SWE');
@@ -32,19 +32,18 @@ async function countStudents(Path) {
       console.log(msg);
       resolve(msg);
     });
-    
   });
 }
 
 if (require.main === module) {
-  countStudents("database.csv")
+  countStudents('database.csv')
     .then(() => {
-        console.log("Done!");
+      console.log('Done!');
     })
-        .catch((error) => {
-        console.log(error);
+    .catch((error) => {
+      console.log(error);
     });
-  console.log("After!");
+  console.log('After!');
 }
 
 module.exports = countStudents;
